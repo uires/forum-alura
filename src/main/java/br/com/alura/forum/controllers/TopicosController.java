401a47f62c2dbe4daac26f2d6aa4403c817a3165
uires <uiresdeivide1@gmail.com>
2019-06-18 22:03:20 -0300

    validando objetos

--- src/main/java/br/com/alura/forum/controllers/TopicosController.java
+++ src/main/java/br/com/alura/forum/controllers/TopicosController.java
@@ -3,6 +3,8 @@ package br.com.alura.forum.controllers;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,7 +40,7 @@ public class TopicosController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<TopicoDTO> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriComponentsBuilder) {
+	public ResponseEntity<TopicoDTO> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriComponentsBuilder) {
 		Topico topico = topicoRepository.save(form.converter(this.cursoRepository));
 		URI uri = uriComponentsBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 		return ResponseEntity.created(uri).body(new TopicoDTO(topico));
--- src/main/java/br/com/alura/forum/controllers/forms/TopicoForm.java
+++ src/main/java/br/com/alura/forum/controllers/forms/TopicoForm.java
@@ -1,12 +1,26 @@
 package br.com.alura.forum.controllers.forms;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
 import br.com.alura.forum.models.Curso;
 import br.com.alura.forum.models.Topico;
 import br.com.alura.forum.repositorys.CursoRepository;
 
 public class TopicoForm {
+	
+	@NotNull
+	@NotEmpty
+	@Length(min = 5)
 	private String titulo;
+	@NotNull
+	@NotEmpty
+	@Length(min = 5)
 	private String mensagem;
+	@NotNull
+	@NotEmpty
 	private String nomeCurso;
 
 	public String getTitulo() {
