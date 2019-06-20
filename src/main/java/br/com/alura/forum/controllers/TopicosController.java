4ced705664f8039ceb251ead377254139ac9a363
uires <uiresdeivide1@gmail.com>
2019-06-20 15:55:37 -0300

    cache, alteração do pom e aplicando cache no endpoint de listagem

--- src/main/java/br/com/alura/forum/controllers/TopicosController.java
+++ src/main/java/br/com/alura/forum/controllers/TopicosController.java
@@ -7,6 +7,7 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
@@ -58,6 +59,7 @@ public class TopicosController {
 	*/
 	
 	@GetMapping
+	@Cacheable(value = "listaDeTopicos")	
 	public Page<TopicoDTO> lista(@RequestParam(name = "nomeCurso", required = false) String nomeCurso,
 			@PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable pageable) {
 		if (nomeCurso == null) {
